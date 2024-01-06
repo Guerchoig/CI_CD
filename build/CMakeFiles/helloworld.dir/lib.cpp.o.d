8337536d6d8ef06cbff72da2a8a2b095082f0b44
guerchoig <guerchoig@gmail.com>
2024-01-06 13:32:27 +0300

    After build

---
+++ build/CMakeFiles/helloworld.dir/lib.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/helloworld.dir/lib.cpp.o: \
+ /home/sasha/source/OtusCC/CI_CD/lib.cpp /usr/include/stdc-predef.h \
+ /home/sasha/source/OtusCC/CI_CD/lib.h \
+ /home/sasha/source/OtusCC/CI_CD/build/version.h
