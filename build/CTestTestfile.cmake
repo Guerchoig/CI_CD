164797b5bfef7846e7f73c245b2f1b12aa615805
guerchoig <guerchoig@gmail.com>
2024-01-06 13:06:24 +0300

    Msg1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sasha/source/OtusCC/CI_CD
+# Build directory: /home/sasha/source/OtusCC/CI_CD/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/sasha/source/OtusCC/CI_CD/CMakeLists.txt;74;add_test;/home/sasha/source/OtusCC/CI_CD/CMakeLists.txt;0;")
